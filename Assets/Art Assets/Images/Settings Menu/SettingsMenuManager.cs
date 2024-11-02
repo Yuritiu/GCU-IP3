7d768003c153cd9971e7f831cc37af2ece216fae
jacklawrence <122370806+Jack-Lawrence@users.noreply.github.com>
2024-11-02 18:19:27 +0000

    V0.2.1.4
    Settings menu starting to be implemented more. Still hooking up to do will discuss controls + game later on

--- Assets/Art Assets/Images/Settings Menu/SettingsMenuManager.cs
+++ Assets/Art Assets/Images/Settings Menu/SettingsMenuManager.cs
@@ -1,12 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsMenuManager : MonoBehaviour
 {
     [Header("Menus")]
     public GameObject mainMenuParent;
     public GameObject settingsMenuParent;
+
+    [Header("Sub Menus")]
+    public GameObject gameSubMenu;
+    public GameObject controlsSubMenu;
+    public GameObject videoSubMenu;
+    public GameObject audioSubMenu;
+
+    [Header("Buttons")]
+    public Button gameButton;
+    public Button controlsButton;
+    public Button videoButton;
+    public Button audioButton;
+
+    void Start()
+    {
+        gameButton.onClick.AddListener(() => ShowSubMenu(gameSubMenu));
+        controlsButton.onClick.AddListener(() => ShowSubMenu(controlsSubMenu));
+        videoButton.onClick.AddListener(() => ShowSubMenu(videoSubMenu));
+        audioButton.onClick.AddListener(() => ShowSubMenu(audioSubMenu));
+    }
 
     void Update()
     {
@@ -23,5 +44,15 @@ public class SettingsMenuManager : MonoBehaviour
     {
         settingsMenuParent.SetActive(false);
         mainMenuParent.SetActive(true);
+    }
+
+    void ShowSubMenu(GameObject subMenuToShow)
+    {
+        gameSubMenu.SetActive(false);
+        controlsSubMenu.SetActive(false);
+        videoSubMenu.SetActive(false);
+        audioSubMenu.SetActive(false);
+
+        subMenuToShow.SetActive(true);
     }
 }

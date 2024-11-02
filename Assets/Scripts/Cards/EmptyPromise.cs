671afa25b5de2ba76c523ded66f4089c2df2ff98
Frostegy <jackgrier2004@gmail.com>
2024-11-02 23:16:40 +0000

    V0.2.1.5
    Re Added Empty Promise Card to game

---
+++ Assets/Scripts/Cards/EmptyPromise.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmptyPromise : MonoBehaviour
+{
+    public void PlayCardForPlayer()
+    {
+        //Places Card But Does Nothing
+        return;
+    }
+    public void PlayCardForAI()
+    {
+        //Places AI's Card But Does Nothing
+        return;
+    }
+
+    //Side Effects To be added
+}

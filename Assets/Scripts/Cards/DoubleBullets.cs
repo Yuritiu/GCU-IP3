1efa05959dc132792de733c1ca6474824f85f8ed
Yuritiu <benfin58@gmail.com>
2024-11-05 23:51:52 +0000

    V0.2.3.7
    Added 2 in the chamber backfire

--- Assets/Scripts/Cards/DoubleBullets.cs
+++ Assets/Scripts/Cards/DoubleBullets.cs
@@ -6,11 +6,30 @@ public class DoubleBullets : MonoBehaviour
 {
     public void PlayCardForPlayer()
     {
+        int rand = Random.Range(0, 5);
+        //\/Debuging\/
+        //rand = 0;
+        //print(rand);
+        if (rand == 0)
+        {
+            //loads blank (does nothing)
+            GameManager.Instance.twoInChamberBackfire.gameObject.SetActive(true);
+            return;
+        }
         GameManager.Instance.addBullet();
     }
 
     public void PlayCardForAI()
     {
+        int rand = Random.Range(0, 5);
+        //\/Debuging\/
+        //rand = 0;
+        //print(rand);
+        if (rand == 0)
+        {
+            //loads blank (does nothing)
+            return;
+        }
         GameManager.Instance.addBullet();
     }
 }

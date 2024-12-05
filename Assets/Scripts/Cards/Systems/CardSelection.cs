41871c14dc2725f7435cb70e8295988b5628fb4c
jacklawrence <122370806+Jack-Lawrence@users.noreply.github.com>
2024-12-05 15:21:55 +0000

    V0.3.8.1
    Card Tooltips - Not exactly working but should be easily fixable

--- Assets/Scripts/Cards/Systems/CardSelection.cs
+++ Assets/Scripts/Cards/Systems/CardSelection.cs
@@ -1,41 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class CardSelection : MonoBehaviour
 {
-    //!-Coded By Charlie-!
-
-    [Header("References")]
-    [SerializeField] string cardInfoImageTag = "CardInfoImage";
-    [SerializeField] string cardInfoTextTag = "CardInfoText";
-
     [Header("Info Variables")]
-    [SerializeField] Image cardInfoImage = null;
-    [SerializeField] string cardInfoText = "*Insert Card Information*";
+    [SerializeField] private TextMeshProUGUI cardInfoText;
 
     [HideInInspector] public bool canSelect = true;
 
     void Start()
     {
-
+        if (cardInfoText != null)
+            cardInfoText.gameObject.SetActive(false);
     }
 
     public void CardSelected()
     {
-    
     }
 
     public void CardHovered(bool hovering)
     {
-        if (CardDrawSystem.Instance.cardMoving)
+        if (CardDrawSystem.Instance.cardMoving || cardInfoText == null)
             return;
 
-        //!-MUST SET CardInfoImage & CardInfoText IN THE UI CANVAS TAGS TO THE TAGS DEFINED IN THE REFERENCES ABOVE-!
-        var cardImage = GameObject.FindGameObjectWithTag(cardInfoImageTag).GetComponent<Image>();
-        var cardText = GameObject.FindGameObjectWithTag(cardInfoTextTag).GetComponent<TextMeshProUGUI>();
+        cardInfoText.gameObject.SetActive(hovering);
     }
 }
+

428dea2b567e252afd84d9f842aa94c6ba4253f3
midnightcereal <144599122+midnightcereal@users.noreply.github.com>
2025-02-13 09:59:16 +0000

    V0.4.8.2
    - Added fov logic

--- Assets/Scripts/Settings/VideoSettings.cs
+++ Assets/Scripts/Settings/VideoSettings.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class VideoSettings : MonoBehaviour
 {
+    [Header("FOV Variables")]
     [SerializeField] Slider fovSlider;
     [SerializeField] TextMeshProUGUI fovValueText;
+    [SerializeField] Camera camera;
 
     float savedFOV;
 
@@ -21,6 +25,11 @@ public class VideoSettings : MonoBehaviour
         savedFOV = fovSlider.value;
         fovValueText.text = savedFOV.ToString();
         PlayerPrefs.SetFloat("FOV", savedFOV);
+
+        if(camera != null)
+        {
+            camera.fieldOfView = savedFOV;
+        }
     }
 
     public void LoadFOV()

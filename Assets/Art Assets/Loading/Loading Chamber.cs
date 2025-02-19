a10fd3ddf6bfae9d82ecb73b3edb35d8782eb19f
jacklawrence <122370806+Jack-Lawrence@users.noreply.github.com>
2025-02-19 21:52:10 +0000

    V0.5.0.0
    Tip text for loading screen

--- Assets/Art Assets/Loading/Loading Chamber.cs
+++ Assets/Art Assets/Loading/Loading Chamber.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using TMPro;
 
 public class LoadingChamber : MonoBehaviour
 {
@@ -12,6 +13,12 @@ public class LoadingChamber : MonoBehaviour
     [SerializeField] private float longSpinDuration = 1f;
     [SerializeField] private float longSpinSpeed = 700f;
 
+    [Header("Tip Display")]
+    [SerializeField] private TextMeshProUGUI tipText;
+    [SerializeField] private string[] tips;
+    [SerializeField] private float tipWaveSpeed = 2f;
+    [SerializeField] private float tipWaveAmplitude = 10f;
+
     private int currentBulletIndex = 0;
     private Transform[] bullets;
 
@@ -23,6 +30,13 @@ public class LoadingChamber : MonoBehaviour
         {
             bullets[i] = chamber.GetChild(i);
             bullets[i].gameObject.SetActive(false);
+        }
+
+        if (tips.Length > 0)
+        {
+            tipText.gameObject.SetActive(true);
+            tipText.text = "Tip: " + tips[Random.Range(0, tips.Length)];
+            StartCoroutine(AnimateTip());
         }
 
         StartCoroutine(LoadingLoop());
@@ -115,4 +129,54 @@ public class LoadingChamber : MonoBehaviour
 
         chamber.rotation = Quaternion.Euler(0, 0, targetRotation);
     }
+
+    private IEnumerator AnimateTip()
+    {
+        tipText.ForceMeshUpdate();
+        TMP_TextInfo textInfo = tipText.textInfo;
+        Vector3[][] originalVertices = new Vector3[textInfo.meshInfo.Length][];
+
+        for (int i = 0; i < textInfo.meshInfo.Length; i++)
+        {
+            originalVertices[i] = (Vector3[])textInfo.meshInfo[i].vertices.Clone();
+        }
+
+        float time = 0f;
+
+        while (true)
+        {
+            tipText.ForceMeshUpdate();
+            textInfo = tipText.textInfo;
+
+            for (int i = 0; i < textInfo.characterCount; i++)
+            {
+                TMP_CharacterInfo charInfo = textInfo.characterInfo[i];
+                if (!charInfo.isVisible)
+                    continue;
+
+                int materialIndex = charInfo.materialReferenceIndex;
+                int vertexIndex = charInfo.vertexIndex;
+                Vector3[] vertices = textInfo.meshInfo[materialIndex].vertices;
+
+                //Create a wave effect on the Y axis Only
+                float yOffset = Mathf.Sin(time + i * 0.3f) * tipWaveAmplitude;
+
+                vertices[vertexIndex + 0] = originalVertices[materialIndex][vertexIndex + 0] + new Vector3(0, yOffset, 0);
+                vertices[vertexIndex + 1] = originalVertices[materialIndex][vertexIndex + 1] + new Vector3(0, yOffset, 0);
+                vertices[vertexIndex + 2] = originalVertices[materialIndex][vertexIndex + 2] + new Vector3(0, yOffset, 0);
+                vertices[vertexIndex + 3] = originalVertices[materialIndex][vertexIndex + 3] + new Vector3(0, yOffset, 0);
+            }
+
+            for (int i = 0; i < textInfo.meshInfo.Length; i++)
+            {
+                textInfo.meshInfo[i].mesh.vertices = textInfo.meshInfo[i].vertices;
+                tipText.UpdateGeometry(textInfo.meshInfo[i].mesh, i);
+            }
+
+            time += Time.deltaTime * tipWaveSpeed;
+            yield return null;
+        }
+    }
+
+
 }

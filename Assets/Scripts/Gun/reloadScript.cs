1a4e705e5dee81a3591a5ba9fc7aa197b6864d09
Jinnes04 <125297175+Jinnes04@users.noreply.github.com>
2025-02-24 14:02:17 +0000

    V0.5.0.8

--- Assets/Scripts/Gun/reloadScript.cs
+++ Assets/Scripts/Gun/reloadScript.cs
@@ -12,6 +12,8 @@ public class reloadScript : MonoBehaviour
     public static reloadScript Instance;
     public Animator gunAnim;
     [SerializeField] public float speed;
+    public GameObject gun;
+    [SerializeField] ShootScript shootScript;
 
     public bool reloadHappened;
     public GameObject chamber;
@@ -32,16 +34,20 @@ public class reloadScript : MonoBehaviour
     {
         if (in1stPos == true)
         {
-            StartCoroutine(gunTransition(targetPos));
             in2ndPos=true;
             in1stPos= false;
+            //shootScript.gunAnim.Play("GunPause");
+            StartCoroutine(gunTransition(targetPos));
+
+            
         }
 
         else if (in2ndPos == true)
         {
-            StartCoroutine(gunTransition(gunPos));
             in1stPos=true;
             in2ndPos=false;
+            //shootScript.gunAnim.Play("GunPause");
+            StartCoroutine(gunTransition(gunPos));
         }
     }
     
@@ -50,15 +56,21 @@ public class reloadScript : MonoBehaviour
     IEnumerator gunTransition(GameObject Target)
     {
         float t = 0.00f;
-        Vector3 startingpos = gameObject.transform.position;
+        Vector3 startingpos = gun.transform.position;
+        bool moveFinished = false;
 
-        while (t < 1.0f)
+        while (t < 1.0f && moveFinished == false )
         {
             t += Time.deltaTime * (Time.timeScale * speed);
-            gameObject.transform.position = Vector3.Lerp(startingpos, Target.transform.position, t);
+            gun.transform.position = Vector3.Lerp(startingpos, Target.transform.position, t);
+            gun.transform.rotation = Quaternion.Slerp(gun.transform.rotation, Target.transform.rotation, speed * Time.deltaTime);
+
+            if (t >= 1.0f)
+                moveFinished = true;
 
             yield return null;
         }
+        
 
         
     }

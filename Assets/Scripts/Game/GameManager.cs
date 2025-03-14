82d8556af307e076c98cec5d1b2bac3073c54a72
Jinnes04 <125297175+Jinnes04@users.noreply.github.com>
2025-03-14 14:55:03 +0000

    V0.5.7.2
    - gun doesn't play at the same time

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -575,6 +575,7 @@ public class GameManager : MonoBehaviour
 
         if (gun.name == "PlayerGun" && !playerGunActive)
         {
+            playerGunActive = true;
             float t = 0.00f;
             while (t < 1.00f)
             {
@@ -584,7 +585,7 @@ public class GameManager : MonoBehaviour
                 yield return null;
             }
             has2Guns = false;
-            playerGunActive = true;
+            
             Gun.SetActive(false);
             Gun.transform.rotation = startRotation;
             Gun.transform.position = startPosition;
@@ -600,6 +601,7 @@ public class GameManager : MonoBehaviour
 
         if (gun.name == "AiGun" && !playerGunActive && !aiGunActive)
         {
+            aiGunActive = true;
             float t = 0.00f;
             while (t < 1.00f)
             {
@@ -610,7 +612,7 @@ public class GameManager : MonoBehaviour
             }
 
             has2Guns = false;
-            aiGunActive = true;
+            
             Gun.SetActive(false);
             Gun.transform.rotation = startRotation;
             Gun.transform.position = startPosition;

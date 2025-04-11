4756da204a8b91dca1e8da5b80694404decd2c90
midnightcereal <144599122+midnightcereal@users.noreply.github.com>
2025-04-11 20:21:35 +0100

    V0.6.5.8 one step closer
    - Fixed perma backfire on bottle

--- Assets/Scripts/Cards/BottleCard.cs
+++ Assets/Scripts/Cards/BottleCard.cs
@@ -71,7 +71,7 @@ public class BottleCard : MonoBehaviour
         float chance = gameManager.statusPercent;
         float roll = UnityEngine.Random.Range(0f, 100f);
 
-        if (roll <= 100)
+        if (roll <= chance)
         {
             disableUI.Instance.DisableAllText();
             blur = GameObject.Find("Blur");
